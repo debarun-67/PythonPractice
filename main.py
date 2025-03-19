f72fecec4799b5d13bf479140fdce9de769db346
Debarun Biswas <rw.68.work@gmail.com>
2025-03-20 00:47:03 +0530

    h

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print
+
+adjective1 = input("enter an adhjective (description)")
+noun1 = input("Enter a noun (person, place, thing): ")
+adjective2 = input("Enter an adjective (description):")
+verb1 = input("Enter a verb ending with 'ing' ")
+adjective3 = input ("Enter an adjective (description): ")
+
+
+print(f"Today I went to a {adjective1} zoo")
+print(f"In an exhibit, I say a {noun1}")
+print(f"{noun1} was {adjective2} and {verb1}")
+print(f"I was {adjective3}!")
