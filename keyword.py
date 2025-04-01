8518a5cbc92fa05d80e4754ad85274b89f5d7ac3
Debarun Biswas <rw.68.work@gmail.com>
2025-04-01 20:07:22 +0530

    d

--- keyword.py
+++ keyword.py
@@ -1,2 +1,29 @@
+# def hello(greeting, title, first, last):
+#     print(f"{greeting} {title}{first}  {last}")
+#
+# hello("hello", title="Mr.", first="Spongebob", last=" thie")
 
 
+def add(*args):
+    total = 0
+    for arg in args:
+        total += arg
+    return total
+
+
+print(add(1,2, 4, 58))
+
+
+def display_name(*args):
+    for arg in args:
+        print(arg, end=" ")
+
+display_name("spongebob", "squarepants")
+
+
+
+def print_address(**kwargs):
+    for key, value in kwargs.items():
+        print(f"{key}: {value}")
+
+print_address(street="shimultala", city="krishanangar", state="west bengal", zip="741101")
